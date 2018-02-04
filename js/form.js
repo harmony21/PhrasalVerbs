76d59fda831d4e075b6411e2d22995c2c3a7d081
Inna Botvinnik <21icelady97@gmail.com>
2018-02-04 20:36:30 +1000

    adaptive version with form, without pay link

---
+++ js/form.js
@@ -0,0 +1,6 @@
+function ml_webform_success_7600364() {
+                var $ = ml_jQuery || jQuery;
+
+                $('.ml-subscribe-form-7600364 .ml-block-success').show();
+                $('.ml-subscribe-form-7600364 .ml-block-form').hide();
+            };
